17447b30d837b3d101d7456581d542883c7d1ad3
Wolf <d.wolftripp@gmail.com>
2022-10-26 13:04:12 -0700

    add connection notice

--- config/database.js
+++ config/database.js
@@ -1,3 +1,9 @@
 import mongoose from 'mongoose'
 
 mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection
+
+db.on('connected', function(){
+  console.log(`Connected to MonogoDB ${db.name} at ${db.host}:${db.port}`)
+})
