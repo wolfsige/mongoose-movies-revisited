7af221e6526fef71e9fba90606fde8f82e6b0774
Wolf <d.wolftripp@gmail.com>
2022-10-26 13:32:25 -0700

    add movieSchema

---
+++ models/movie.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+  title: String,
+  releaseYear: Number,
+  MpaaRating: String,
+  cast:[String],
+})
